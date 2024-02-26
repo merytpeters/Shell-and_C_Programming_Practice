3faf8c9a694547b85fa8b09c8d27ab0dbb828f38
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-26 15:09:49 +0100

    Modified to take in only 3 digit and above

--- Control_flow/leap_year.c
+++ Control_flow/leap_year.c
@@ -11,15 +11,20 @@ int main(void)
 	printf("Enter the year: ");
 	scanf("%d", &year);
 
-	if (year % 2 == 0)
+	if (year >= 100)/*known years start from 100 B.C or A.D*/
 	{
-		if (year % 4 == 0)
-			printf("%d is a leap year\n", year);
+		if (year % 2 == 0)
+		{
+			if (year % 4 == 0)
+				printf("%d is a leap year\n", year);
+			else
+				printf("%d is two years from a leap year\n", year);
+		}	
 		else
-			printf("%d is two years from a leap year\n", year);
+		printf("%d is not a leap year\n", year);
 	}
 	else
-		printf("%d is not a leap year\n", year);
+		printf("%d is not a known calendar year\n", year);
 
 	return (0);
 }
